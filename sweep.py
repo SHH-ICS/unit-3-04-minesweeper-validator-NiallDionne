268abe763f1e153581cf3130263becd436dde15b
NiallDionne <144242112+NiallDionne@users.noreply.github.com>
2023-11-14 14:01:16 +0000

    code cant run

--- sweep.py
+++ sweep.py
@@ -5,13 +5,26 @@
 def validate( block_data ):
   # Check whether the centre block is a bomb, a number, or an invalid input
   # Skip bombs, send an error on invalid input, verify numbers
+  num=grid[1][1]
+  b = 0
+  if num == -1:
+    for i in range(3):
+      for j in range(3):
+        if (i, j) == (1, 1):
+          continue
+        if num == -1:
+          b = b + 1
+
+    if num == b:
+      print("valid middle block")
+    else:
+      print("invalid middle block")
 
   return
 
-
 grid = [
-  [-1,1,0],
-  [1,1,0],
-  [0,0,0]
+  [1,1,1],
+  [1,-1,1],
+  [1,1,1]
 ]
 print (validate(grid))
